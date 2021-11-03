ca3f94c55bd58160debc945af1b943d4a8ea17cd
esther-v <esvalas@gmail.com>
2021-11-03 18:52:21 +0100

    get and display data

--- src/queries.js
+++ src/queries.js
@@ -1,18 +1,19 @@
 import { gql } from '@apollo/client'
 
-const getAllCharacters = gql`{
-    characters {
-        results {
-            id
-            name
-            image
-            status
-            species
-            location {
+const GET_ALL_CHARACTERS = gql`
+    query Character($page: Int){
+        characters(page: $page) {
+            results {
+                id
                 name
+                image
+                status
+                species
+                location {
+                    name
+                }
             }
         }
-    }
 }`
 
-export {getAllCharacters}
+export {GET_ALL_CHARACTERS}
